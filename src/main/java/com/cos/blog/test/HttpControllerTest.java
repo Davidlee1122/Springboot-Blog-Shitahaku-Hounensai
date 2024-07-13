04434cabb09a64416cb0e0aac7ad6e28e27ca25e
Jaehuhn Lee <davidlee1122@naver.com>
2024-07-13 13:33:10 +0900

    2024/07/13 fin

--- src/main/java/com/cos/blog/test/HttpControllerTest.java
+++ src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -9,10 +9,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 //사용자가 요청 -> 응답(data)
-
 @RestController
 public class HttpControllerTest {
 
+	private static final String TAG = "HttpControllerTest : ";
+	
+	@GetMapping("/http/lombok")
+	public String lombokTest() {
+		Member m = Member.builder().username("ssar").password("1234").email("ssar@nate.com").build();
+		System.out.println(TAG+"getter : "+m.getUsername());
+		m.setUsername("cos");
+		System.out.println(TAG+"getter : "+m.getUsername());
+		return "lombok test 완료";
+	}
 	//인터넷 브라우저 요청은 무조건 get요청밖에 할 수 없다.
 	//http://localhost:8080/http/get (select)
 	@GetMapping("/http/get")
--- src/main/java/com/cos/blog/test/Member.java
+++ src/main/java/com/cos/blog/test/Member.java
@@ -1,11 +1,21 @@
 package com.cos.blog.test;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class Member {
 	private int id;
 	private String username;
 	private String password;
 	private String email;
-		
+	
+	@Builder
+	//builder를 쓰지 않는 경우 일일이 오버로딩을 해서 맞는 member를 만들어줘야함.
+	//또한 순서를 맞추지 않아도 그 이름 그대로에 매칭을 시키기 때문에 훨씬 더 편리함.
 	public Member(int id, String username, String password, String email) {
 		this.id = id;
 		this.username = username;
@@ -13,30 +23,6 @@ public class Member {
 		this.email = email;
 	}
 	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	
 	
 }
+
