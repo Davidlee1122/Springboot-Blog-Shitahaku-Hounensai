f9f74ac8fcd6301f06f295c9b7faccbf9a32db4e
Jaehuhn Lee <davidlee1122@naver.com>
2024-07-18 18:01:25 +0900

    2024/07/18 fin

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -3,6 +3,8 @@ package com.cos.blog.test;
 import java.util.List;
 import java.util.function.Supplier;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,6 +13,8 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.blog.model.RoleType;
@@ -22,6 +26,32 @@ public class DummyControllerTest {
 
 	@Autowired // 의존성 주입(DI)
 	private UserRepository userRepository;
+	
+	//email, password만 수정이 가능하게 할거니까 둘을 입력 받아야함 
+	@Transactional //이거 붙이면 save하지 않아도 update가 된다.
+	//위의 transactional은 함수가 종료될 때 자동으로 commit을 진행함(원래의 영속화된 값들과의 변경을 감지한다)
+	@PutMapping("/dummy/user/{id}")
+	public User updateUser(@PathVariable int id, @RequestBody User requestUser) {//json 데이터를 요청했지만, Java object로 (MessageConverter의 Jackson 라이브러리)가 전환해서 받아줌
+		System.out.println("id : "+id);
+		System.out.println("password : "+requestUser.getPassword());
+		System.out.println("email : "+requestUser.getEmail());
+		//save는 보통 insert할 때 사용 update할 때 사용할 경우 바꾸지 않는 녀석들을 전부 null로 바꿔버리는 대참사가 발생
+		
+		User user = userRepository.findById(id).orElseThrow(()->{
+			return new IllegalArgumentException("수정에 실패하였습니다");
+		});
+		user.setPassword(requestUser.getPassword());
+		user.setEmail(requestUser.getEmail());
+				
+		//userRepository.save(user);
+		//save함수는 id를 전달하지 않으면 insert를 해주고
+		//save함수는 id를 전달하면 해당 id에 대한 데이터가 있으면 update를 해주고
+		//save함수는 id를 전달하면 해당 id에 대한 데이터가 없으면 insert를 해요.
+		
+		//더티 체킹
+		//위의 transactional은 함수가 종료될 때 자동으로 commit을 진행함(원래의 영속화된 값들과의 변경을 감지한다)
+		return null;
+	}
 	
 	//http://localhost:8000/blog/dummy/user
 	@GetMapping("/dummy/users")
