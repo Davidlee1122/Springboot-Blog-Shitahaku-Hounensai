e80f347bfc3cb63b85389ef70f69ed050bf5b361
Jaehuhn Lee <davidlee1122@naver.com>
2024-07-19 19:22:28 +0900

    2024/07/19 fin

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -6,10 +6,12 @@ import java.util.function.Supplier;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,6 +28,17 @@ public class DummyControllerTest {
 
 	@Autowired // 의존성 주입(DI)
 	private UserRepository userRepository;
+	
+	@DeleteMapping("/dummy/user/{id}")
+	public String delete(@PathVariable int id) {
+		try {
+			userRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			return "삭제에 실패하였습니다. 해당 id는 DB에 없습니다.";
+		}
+		
+		return "삭제되었습니다. id : "+id;
+	}
 	
 	//email, password만 수정이 가능하게 할거니까 둘을 입력 받아야함 
 	@Transactional //이거 붙이면 save하지 않아도 update가 된다.
@@ -50,7 +63,7 @@ public class DummyControllerTest {
 		
 		//더티 체킹
 		//위의 transactional은 함수가 종료될 때 자동으로 commit을 진행함(원래의 영속화된 값들과의 변경을 감지한다)
-		return null;
+		return user;
 	}
 	
 	//http://localhost:8000/blog/dummy/user
