a5e22d7f0e8e130fdc157b1a8c277430ec959593
Jaehuhn Lee <davidlee1122@naver.com>
2024-07-25 12:31:47 +0900

    2024/07/25 fin

--- src/main/java/com/cos/blog/service/UserService.java
+++ src/main/java/com/cos/blog/service/UserService.java
@@ -1,9 +1,11 @@
 package com.cos.blog.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.repository.UserRepository;
 
@@ -14,10 +16,17 @@ public class UserService {
 
 	@Autowired
 	private UserRepository userRepository;
+
+	@Autowired
+	private BCryptPasswordEncoder encoder;
 	
 	@Transactional
 	//select를 할 때에도 Transactional을 붙인다 -> 왜? 정합성을 유지하기 위해서
 	public void 会員登録(User user) {
+		String rawPassword = user.getPassword(); //원문 비밀번호
+		String encPassword = encoder.encode(rawPassword); // 해쉬화
+		user.setPassword(encPassword);
+		user.setRole(RoleType.USER);
 		userRepository.save(user);
 	}
 	
