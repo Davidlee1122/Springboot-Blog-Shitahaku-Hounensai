c8a0319ace663b6e20152b6ddea53009f2f29a47
Jaehuhn Lee <davidlee1122@naver.com>
2024-07-25 13:59:11 +0900

    2024/07/25 fin

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -20,7 +20,7 @@ public class UserApiController {
 	@Autowired
 	private UserService userService;
 	
-		
+	 
 	@PostMapping("/auth/joinProc")
 	public ResponseDto<Integer> save(@RequestBody User user) {
 		System.out.println("UserApiController : save 호출됨"); 
